2d944fc54ec73371848be9b117cc796ce1f600e9
Dana Triz <github@danatriz.com>
2023-07-18 22:05:57 +0700

    update index.vue and TextInput.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,14 +12,48 @@
 
             <div>
                 <TextInput 
-                    placehoder="Email: your@email.com"
+                    placeholder="Email: your@email.com"
                     v-model:input="email"
                     inputType="email"
                     :error="error && error.mail ? error.email[0]:''"
                 />
             </div>
 
+            <div class="mt-4">
+                <TextInput 
+                    placeholder="Password"
+                    v-model:input="password"
+                    inputType="password"
+                    :error="error && error.password ? error.password[0]:''"
+                />
+            </div>
+
+            <div class="mt-10">
+                <button
+                type='submit'
+                class="rounded-full w-full p-3 font-bold"
+                :disabled="(!email || !password)"
+                :class="
+                    (email && password)
+                    ? 'bg-[#b228D9] hover-bg-[#5c21b3] text-white'
+                    : 'bg-[#EFF0eB] text-[#A7AAA2]'
+                "
+                >
+                    Log in
+                </button>
+            </div>
+
             </form>
+
+            <div class="text-[14px] text-center pt-12">
+                Don't have an account?
+                <NuxtLink
+                    to="/register"
+                    class="text-[#8228D9] underline"
+                >
+                    Sign Up
+                </NuxtLink>
+            </div>
         </div>
     </AuthLayout>
 </template>
